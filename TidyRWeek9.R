54fbdcfde3a982e1c8c00c76d13e751628014b2c
Rachel Roday <77400912+rroday@users.noreply.github.com>
2021-02-24 10:25:53 -0500

    Update TidyRWeek9.R

--- TidyRWeek9.R
+++ TidyRWeek9.R
@@ -28,13 +28,6 @@ library(dplyr)
 library(ggrepel)
 
 # Get the Data
-
-# Read in with tidytuesdayR package 
-# Install from CRAN via: install.packages("tidytuesdayR")
-# This loads the readme and all the datasets for the week of interest
-
-# Either ISO-8601 date or year/week works!
-
 tuesdata <- tidytuesdayR::tt_load('2021-02-23')
 
 earn <- tuesdata$earn
